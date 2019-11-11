58a1e3bc776392f83b6f4f8684a8cccf3ddeacee
akityty <congnv.9803@gmail.com>
2019-11-11 15:37:38 +0700

    add users models

---
+++ src/main/java/com/codegym/freshfood/model/users.java
@@ -0,0 +1,49 @@
+package com.codegym.freshfood.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class users {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  @Email
+  private String email;
+  @Size(min = 6, max = 20)
+  private String password;
+
+  public users() {
+  }
+
+  public users(@Email String email, @Size(min = 6, max = 20) String password) {
+    this.email = email;
+    this.password = password;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
